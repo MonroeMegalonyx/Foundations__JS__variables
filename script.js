e9f355b8495ab51e8419edbe969672951a520753
Vincent OLeary <vjoleary@users.noreply.github.com>
2020-12-10 18:20:16 -0500

    NewForce exercises on variables and data types

---
+++ script.js
@@ -0,0 +1,15 @@
+//alert("Hello, world")
+
+console.log("howdy, world!")
+
+let myName = "Vincent"
+
+console.log(myName)
+
+myName = "Jacob"
+
+console.log(myName)
+
+let movieArray = ["Avengers", "The Crown"]
+console.log(movieArray[0])
+console.log(movieArray[1])
